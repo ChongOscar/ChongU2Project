c5a157c30a40d38c071b30f97f1d181fa6d9aa1b
BT_1N3_24 <sample@bths.edu>
2023-10-17 10:51:11 -0400

    finish equaton

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -11,27 +11,39 @@ public class LinearEquation {
     }
 
     public double distance() {
-        return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) * 100.0) / 100.0;
+        return roundToHundredth(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
     }
 
     public double slope() {
-        return Math.round((y2 - y1) / (x2- x1) * 100.0) /100.0;
+        return roundToHundredth( (y2 - y1 * 1.0) / (x2 - x1 * 1.0));
     }
 
     public double yIntercept() {
-        return Math.round((y1 - (slope() * x1)) * 100.0) / 100.0;
+        return roundToHundredth((y1 - (slope() * x1)));
     }
 
     public String equation() {
-        String equation = "y = ";
-        if (slope() % 1 == 0) {
-            equation += slope();
-        } else if (x2 - x1 < 0 && y2 - y1 < 0) {
-            equation += ((y2 - y1) * -1) + "/" + ((x2 - x1) * -1);
-        } else if (x2 - x1 < 0) {
-            equation += "-" + (y2 - y1) + "/" + ((x2 - x1) * -1);
+        if (x1 == x2) {
+            return "x = " + x1;
         }
-        if (yIntercept() < 0) {
+        if (y1 == y2) {
+            return "y = " + y1;
+        }
+        String equation = "y = ";
+        if (slope() == 1) {
+            equation += "x";
+        } else if (slope() == -1) {
+            equation += "-x";
+        } else if (slope() % 1 == 0) {
+            equation += (y2 - y1) + "/" + (x2 - x1) + "x";
+        } else if (x2 - x1 < 0 && y2 - y1 < 0) {
+            equation += ((y2 - y1) * -1) + "/" + ((x2 - x1) * -1) + "x";
+        } else if (x2 - x1 < 0) {
+            equation += "-" + (y2 - y1) + "/" + ((x2 - x1) * -1) + "x";
+        }
+        if (yIntercept() == 0) {
+            equation += "";
+        } else if (yIntercept() < 0) {
             equation += " - " + (yIntercept() * -1);
         } else {
             equation += " + " + yIntercept();
@@ -39,5 +51,22 @@ public class LinearEquation {
 
         return equation;
     }
+
+    public String coordinateForX(double x) {
+        return "(" + x + ", " + (Math.round((slope() * x + yIntercept()) * 100.0) / 100.0) + ")";
+    }
+
+    public String lineInfo() {
+        return "The two points are: (" + x1 + ", " + y1 + ") and (" + x2 + ", " + y2 + ")\n" +
+               "The equation of the line between these points is:" + equation() + "\n" +
+               "The slope of this line is: " + slope() + "\n" +
+               "The y-intercept of this line is: " + yIntercept() + "\n" +
+               "The distance between these points is " + distance();
+    }
+
+    private double roundToHundredth(double toRound) {
+        return Math.round(toRound * 100.0) / 100.0;
+    }
+
 
 }
---
+++ src/LinearEquationLogic.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class LinearEquationLogic {
+    private LinearEquation linearEquation;
+    private Scanner scanner;
+
+    public LinearEquationLogic() {
+        linearEquation = null;
+        scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+
+    }
+
+    private void getData() {
+        System.out.println("E");
+    }
+}
